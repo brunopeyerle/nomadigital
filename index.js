3cf22265dcdfe95d4b96264959f5c301af6d18d4
Bruno Peyerle <brunopeyerle@hotmail.com>
2025-02-26 14:22:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 // Efeito de transição suave para o scroll nas âncoras
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-  
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
-  
-  // Adiciona uma animação ao carregar os vídeos (fade-in)
-  window.addEventListener('scroll', function () {
-    const videos = document.querySelectorAll('.video-item');
-    const windowHeight = window.innerHeight;
-  
-    videos.forEach(video => {
-      const videoTop = video.getBoundingClientRect().top;
-      if (videoTop < windowHeight - 100) {
-        video.classList.add('fade-in');
-      }
-    });
+});
+
+// Adiciona uma animação ao carregar os vídeos (fade-in)
+window.addEventListener('scroll', function () {
+  const videos = document.querySelectorAll('.video-item');
+  const windowHeight = window.innerHeight;
+
+  videos.forEach(video => {
+    const videoTop = video.getBoundingClientRect().top;
+    if (videoTop < windowHeight - 100) {
+      video.classList.add('fade-in');
+    }
   });
-  
+});
